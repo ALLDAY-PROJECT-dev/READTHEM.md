627e04b40b7c69c45bd3066ceaeb98315849552d
mjkim12345 <zmdl96@gmail.com>
2026-06-29 16:29:44 +0900

    feat: Book 엔티티 추가

--- back/src/main/java/com/back/domain/book/entity/Book.java
+++ back/src/main/java/com/back/domain/book/entity/Book.java
@@ -1,9 +1,41 @@
 package com.back.domain.book.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Book extends BaseEntity {
-    //
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(unique = true)
+    private String isbn;
+
+    private String authors;
+
+    private String publishedDate;
+
+    private String publisher;
+
+    private String imgUrl;
+
+    public Book(String title, String description, String isbn,
+                String authors, String publishedDate,
+                String publisher, String imgUrl) {
+        this.title = title;
+        this.description = description;
+        this.isbn = isbn;
+        this.authors = authors;
+        this.publishedDate = publishedDate;
+        this.publisher = publisher;
+        this.imgUrl = imgUrl;
+    }
 }
