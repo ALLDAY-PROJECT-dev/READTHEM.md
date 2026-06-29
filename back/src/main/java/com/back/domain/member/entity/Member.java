60931013a010193dbda6464cff0a6f88f9a1667a
EasyH <skysea001010@gmail.com>
2026-06-29 14:19:00 +0900

    feat: Member 엔티티 작업

--- back/src/main/java/com/back/domain/member/entity/Member.java
+++ back/src/main/java/com/back/domain/member/entity/Member.java
@@ -1,9 +1,51 @@
 package com.back.domain.member.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.UUID;
+
+@NoArgsConstructor
 @Entity
 public class Member extends BaseEntity {
-    //
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String nickname;
+    @Column(unique = true)
+    private String refreshToken;
+
+    public Member(long id, String username, String name) {
+        setId(id);
+        this.username = username;
+        setName(name);
+    }
+
+    public Member(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.refreshToken = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return nickname;
+    }
+    public void setName(String name) {
+        this.nickname = name;
+    }
+
+    public void modifyRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public boolean isAdmin() {
+        if ("system".equals(username)) return true;
+        return "admin".equals(username);
+    }
+
 }
--- back/src/main/java/com/back/domain/member/service/MemberService.java
+++ back/src/main/java/com/back/domain/member/service/MemberService.java
@@ -1,13 +1,20 @@
 package com.back.domain.member.service;
 
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public Optional<Member> findById(Long id) {
+        return memberRepository.findById(id);
+    }
 }
