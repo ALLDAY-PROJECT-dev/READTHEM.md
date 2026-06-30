8d00808c0fd88302c6f50ef2b4ac3c2252bea5cb
mjkim12345 <zmdl96@gmail.com>
2026-06-30 10:42:20 +0900

    feat: BookFetchProgress 엔티티 및Repository 추가
    feat: BookFetchProgress 엔티티 및Repository 추가

---
+++ back/src/main/java/com/back/domain/book/entity/BookFetchProgress.java
@@ -0,0 +1,29 @@
+package com.back.domain.book.entity;
+
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class BookFetchProgress extends BaseEntity {
+
+    private int currentPage;
+    private int currentApiKeyIndex;
+    private boolean completed;
+
+    public void nextPage() {
+        this.currentPage++;
+    }
+
+    public void nextApiKeyIndex() {
+        this.currentApiKeyIndex++;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+}
---
+++ back/src/main/java/com/back/domain/book/repository/BookFetchProgressRepository.java
@@ -0,0 +1,8 @@
+package com.back.domain.book.repository;
+
+import com.back.domain.book.entity.BookFetchProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookFetchProgressRepository extends JpaRepository<BookFetchProgress, Long> {
+    
+}
