7e25311c0a4d52fee820d9180070022a1fafb026
EasyH <skysea001010@gmail.com>
2026-06-30 11:24:05 +0900

    feat: WishRepository, WishService 구현

--- back/src/main/java/com/back/domain/wish/service/WishService.java
+++ back/src/main/java/com/back/domain/wish/service/WishService.java
@@ -1,13 +1,34 @@
 package com.back.domain.wish.service;
 
+import com.back.domain.book.entity.Book;
+import com.back.domain.member.entity.Member;
+import com.back.domain.wish.entity.Wish;
 import com.back.domain.wish.repository.WishRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WishService {
     private final WishRepository wishRepository;
+
+    public List<Wish> findAll() {
+        return wishRepository.findAll();
+    }
+
+    public List<Wish> findByMember(Member actor) {
+        return wishRepository.findByMember(actor);
+    }
+
+    public Wish addWish(Member actor, Book book) {
+        return wishRepository.save(new Wish(actor, book));
+    }
+
+    public void deleteWish(Member actor, Book book) {
+        wishRepository.deleteByMemberAndBook(actor, book);
+    }
 }
