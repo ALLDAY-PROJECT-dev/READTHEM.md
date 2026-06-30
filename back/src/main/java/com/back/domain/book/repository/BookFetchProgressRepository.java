176c67f38525a40f81671e44d0aa9bccbb5d9e53
mjkim12345 <zmdl96@gmail.com>
2026-06-30 12:43:07 +0900

    "feat: BookFetchProgress 엔티티 추가"

--- back/src/main/java/com/back/domain/book/repository/BookFetchProgressRepository.java
+++ back/src/main/java/com/back/domain/book/repository/BookFetchProgressRepository.java
@@ -4,5 +4,4 @@ import com.back.domain.book.entity.BookFetchProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookFetchProgressRepository extends JpaRepository<BookFetchProgress, Long> {
-    
 }
