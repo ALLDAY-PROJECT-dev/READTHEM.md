1456198469ff9c786efcccd92c160c046dd02ef0
puppywimy <eric250@naver.com>
2026-06-30 16:13:19 +0900

    fix: ApiV1WidgetController RequestMapping을 올바르게 수정 "/members" -> "/widgets"

--- back/src/main/java/com/back/domain/widget/controller/ApiV1WidgetController.java
+++ back/src/main/java/com/back/domain/widget/controller/ApiV1WidgetController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/v1/widgets")
 @RequiredArgsConstructor
 public class ApiV1WidgetController {
     private final WidgetService widgetService;
